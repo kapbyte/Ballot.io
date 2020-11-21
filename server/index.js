765539a1ad3d0961d8a881ed8dfe8ed2ef91c61b
ke1echi <mrkelechichinaka@gmail.com>
2020-11-21 15:00:59 +0100

    Deployed client-sde to netlify

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
-
-// Config dotenv
-require('dotenv').config({ path: './src/.env' });
+require('dotenv').config({ path: './src/.env' }); // Config dotenv
 
 const app = express();
 
@@ -25,8 +23,8 @@ app.get('/', (req, res) => {
 
 // Use routes
 app.use('/auth', authRouter);
-app.use('/user', userRouter);
-// app.use('/user', verifyTokenController, userRouter);
+// app.use('/user', userRouter);
+app.use('/user', verifyTokenController, userRouter);
 
 // Connect to MongoDB and start server
 if (process.env.NODE_ENV !== 'test') {
